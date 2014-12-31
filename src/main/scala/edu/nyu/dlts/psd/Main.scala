6a86331fe8e0fb45bbdfbc40d6c2afb5a31e8606
Don <don.mennerich@gmail.com>
2014-12-31 12:02:06 -0500

    implementing cli

--- src/main/scala/edu/nyu/dlts/psd/Main.scala
+++ src/main/scala/edu/nyu/dlts/psd/Main.scala
@@ -1,18 +1,31 @@
 package edu.nyu.dlts.psd
 
-import java.io.File
+import java.io.{ File, PrintWriter }
 import org.apache.commons.cli.{Options, GnuParser}
 
 object Main extends App with PsdTool {
   val options = new Options
-  options.addOption("t", false, "display current time")
+  options.addOption("v", false, "print the full report")
+  options.addOption("o", true, "path to original file")
+  options.addOption("m", true, "path to migrated file")
+  options.addOption("f", false, "write file to output")
+  
   val parser = new GnuParser()
-  val cmd = parser.parse( options, args)
+  val cmd = parser.parse(options, args)
 
-  println("psdtool v0.1.0")
-  if(cmd.hasOption("t")){println(System.currentTimeMillis())}
-  val origFile = new File("src/main/resources/orig.psd")
-  val migrateFile = new File("src/main/resources/migrate.psd")
+  if(!cmd.hasOption("o") || !cmd.hasOption("m")){println("SYNTAX java -jar -o psdfile [original file] -m [migrated file]"); System.exit(1)}
+
+  val origFile = new File(cmd.getOptionValue("o"))
+  val migrateFile = new File(cmd.getOptionValue("m"))
   val results = comparePSDs(origFile, migrateFile)
-  results.foreach{entry => println(entry._1 + ": " + entry._2)}
+  var result = true
+  var errors = Vector.empty[String]
+  results.foreach{entry => 
+  	if(entry._2 == false){
+  		result = false
+  		errors = errors ++ Vector(entry._1)
+  	}
+  }
+
+  println(result)
 }
